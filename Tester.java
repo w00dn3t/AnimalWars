87534f64b50fc2b60ffdf2993c7064c03cc36249
w00dn3t <w00dn3t@gmail.com>
2013-02-07 23:49:23 -0500

    Update Tester.java

--- Tester.java
+++ Tester.java
@@ -4,7 +4,7 @@ import java.util.Scanner;
 public class Tester {
 
 	public static void main(String[] args){
-		AnimalUI UI = new AnimalUI();
+		AnimalUI UI = new AnimalUI(JOptionPane.showInputDialog("Player 1's name?"), JOptionPane.showInputDialog("Player 2's name?"));
 		
 	}
 
